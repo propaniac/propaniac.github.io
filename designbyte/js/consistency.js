9fb618c82022f9c10a181ad81d4156b0a7686592
propaniac <51009055+propaniac@users.noreply.github.com>
2020-12-03 03:45:08 -0800

    Add files via upload

---
+++ designbyte/js/consistency.js
@@ -0,0 +1,109 @@
+// Button group 1
+    // Negative button
+    $('#deny1:button').click(function() {
+        $("#good1").css("display","none");
+        $("#bad1").css("display","inline");
+    });
+
+// Positive button
+    $('#apply1:button').click(function() {
+        $("#good1").css("display","inline");
+        $("#bad1").css("display","none");
+    });
+
+// Button group 2
+// Negative button
+    $('#deny2:button').click(function() {
+        $("#good2").css("display","none");
+        $("#bad2").css("display","inline");
+    });
+
+// Positive button
+    $('#apply2:button').click(function() {
+        $("#good2").css("display","inline");
+        $("#bad2").css("display","none");
+    });
+
+// Button group 3
+// Negative button
+    $('#deny3:button').click(function() {
+        $("#good3").css("display","none");
+        $("#bad3").css("display","inline");
+    });
+
+// Positive button
+    $('#apply3:button').click(function() {
+        $("#good3").css("display","inline");
+        $("#bad3").css("display","none");
+    });
+
+
+// Button group 4
+// Negative button
+    $('#deny4:button').click(function() {
+        $("#good4").css("display","inline");
+        $("#bad4").css("display","none");
+    });
+
+// Positive button
+    $('#apply4:button').click(function() {
+        $("#good4").css("display","none");
+        $("#bad4").css("display","inline");
+    });
+
+
+
+// Button group 1
+    // Negative button
+        $('#inaction:button').click(function() {
+            $("#good1").css("display","none");
+            $("#bad1").css("display","inline");
+        });
+    
+    // Positive button
+        $('#action:button').click(function() {
+            $("#good1").css("display","inline");
+            $("#bad1").css("display","none");
+        });
+
+// Button group 2
+    // Negative button
+        $('#deny:button').click(function() {
+            $("#good2").css("display","none");
+            $("#bad2").css("display","inline");
+        });
+
+    // Positive button
+        $('#commit:button').click(function() {
+            $("#good2").css("display","inline");
+            $("#bad2").css("display","none");
+        });
+
+// Button group 3
+    // Negative button
+        $('#cancel-example:button').click(function() {
+            $("#good3").css("display","none");
+            $("#bad3").css("display","inline");
+        });
+
+    // Positive button
+        $('#confirm-example:button').click(function() {
+            $("#good3").css("display","inline");
+            $("#bad3").css("display","none");
+        });
+
+
+// Button group 4
+    // Negative button
+        $('#stop:button').click(function() {
+            $("#good4").css("display","none");
+            $("#bad4").css("display","inline");
+        });
+
+    // Positive button
+        $('#go:button').click(function() {
+            $("#good4").css("display","inline");
+            $("#bad4").css("display","none");
+        });
+
+

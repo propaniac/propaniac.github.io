e0682b4850a653d3562e79e1ed8ec5b513ab47a4
propaniac <51009055+propaniac@users.noreply.github.com>
2020-12-02 22:46:36 -0800

    Add files via upload

--- designbyte/js/main.js
+++ designbyte/js/main.js
@@ -18,6 +18,8 @@ function headerMenu() {
   }
 
 /* Card/List view buttons */
+
+// List button
     $('#listButton:button').click(function() {
         /* Remove any existing classes from the id's that will be altered */
         $("#termList").removeClass();
@@ -27,6 +29,19 @@ function headerMenu() {
         $("#4").removeClass();
         $("#5").removeClass();
 
+        // Alter the CSS of the icon and paragragh so icon is hidden and paragraph is shown
+
+        $("#1i").css("display","none");
+        $("#1p").css("display","block");
+        $("#2i").css("display","none");
+        $("#2p").css("display","block");
+        $("#3i").css("display","none");
+        $("#3p").css("display","block");
+        $("#4i").css("display","none");
+        $("#4p").css("display","block");
+        $("#5i").css("display","none");
+        $("#5p").css("display","block");
+
         /* Add the new classes to the id's */
         $("#termList").addClass("flexList");
         $("#1").addClass("list");
@@ -35,10 +50,16 @@ function headerMenu() {
         $("#4").addClass("list");
         $("#5").addClass("list");
 
-        /*Hide the button and display the alternative option */
+        /* Hide the button and display the alternative option */
         $("#listButton").css("display","none");
         $("#cardButton").css("display","block");
+
+        // Hide the current indicator and display the new one
+        $("#list-indicator").css("display","block");
+        $("#card-indicator").css("display","none");
     })
+
+    // Card button function
 
     $('#cardButton:button').click(function() {
         /* Remove any existing classes from the id's that will be altered */
@@ -57,7 +78,23 @@ function headerMenu() {
         $("#4").addClass("card");
         $("#5").addClass("card");
 
+        // Alter the CSS of the icon and paragragh so paragraph is hidden and icon is shown
+        $("#1p").css("display","none");
+        $("#1i").css("display","block");
+        $("#2p").css("display","none");
+        $("#2i").css("display","block");
+        $("#3p").css("display","none");
+        $("#3i").css("display","block");
+        $("#4p").css("display","none");
+        $("#4i").css("display","block");
+        $("#5p").css("display","none");
+        $("#5i").css("display","block");
+
         /*Hide the button and display the alternative option */
         $("#cardButton").css("display","none");
         $("#listButton").css("display","block");
+
+        // Hide the current indicator and display the new one
+        $("#list-indicator").css("display","none");
+        $("#card-indicator").css("display","block");
     })
